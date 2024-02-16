b1b8fa9ca40984fcc52c06e813dee64152904b5d
Natan Maia <natanmaiaplays@outlook.com>
2024-02-16 00:30:37 -0300

    camera tweaks

--- scenes/levels/rooms/room.gd
+++ scenes/levels/rooms/room.gd
@@ -1,13 +1,25 @@
 class_name Room
 extends Node2D
 
+@onready var room_area : Area2D = $RoomArea
 @onready var doors = $Doors
 @onready var waves_group = $CombatWaveGroup
 
-
+var player : Player
 
 func _ready():
+	room_area.body_entered.connect(_on_body_entered)
 	waves_group.finished.connect(on_all_waves_finished)
+	
+	remove_child(doors)
+	#waves_group.start_wave()
+
+
+
+func on_room_entered(p:Player):
+	player = p
+	add_child.call_deferred(doors) # locks all entrances
+	room_area.queue_free()
 	waves_group.start_wave()
 
 
@@ -15,3 +27,9 @@ func on_all_waves_finished():
 	# placeholder behavior for deleting doors
 	for child in doors.get_children():
 		child.queue_free()
+	print_debug("Finished room: ", name)
+
+func _on_body_entered(body:Node2D):
+	if not body is Player: return
+	print_debug("Foom entered: ", name)
+	on_room_entered(body as Player)
