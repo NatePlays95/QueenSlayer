feb0c41ada12fcb139e1024661e26db52b8017e7
Natan Maia <natanmaiaplays@outlook.com>
2024-02-18 15:18:21 -0300

    sword slash

--- scenes/players/attacks/sword_slash.gd
+++ scenes/players/attacks/sword_slash.gd
@@ -1,20 +1,5 @@
-extends Node2D
+extends Hitbox
 
-@export var hitbox: Hitbox
-
-@export var SPEED := 2000.0
-
-var velocity: Vector2 = Vector2.ZERO
-
-func set_direction(direction: Vector2):
-	velocity = direction * SPEED
-	rotation = direction.angle()
-	#print(velocity)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	velocity *= 0.9
-	position += velocity * delta
-
-func _on_timer_timeout():
-	queue_free()
+func _ready():
+	super._ready()
+	$AnimatedSprite2D.global_position.y -= 32
