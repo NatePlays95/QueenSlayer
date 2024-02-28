18146c912d09da1f1c085603d81e8f7792b210ca
Maths <55461720+songsilly@users.noreply.github.com>
2024-02-28 13:54:27 -0300

    Update nas músicas. Novos sons

--- scenes/levels/queen_room.gd
+++ scenes/levels/queen_room.gd
@@ -2,6 +2,6 @@ extends Node2D
 
 func _ready():
 	Fade.fade_in()
-	AudioManager.play_music("Fase1.ogg")
+	AudioManager.play_music("Fase2.ogg")
 	#$CombatWaveGroup.finished.connect(_on_all_waves_finished)
 	#$CombatWaveGroup.start_wave()
--- scripts/common/audio_event_handler.gd
+++ scripts/common/audio_event_handler.gd
@@ -7,8 +7,6 @@ func audio_event_handle(event: String) -> void:
 func _queue_random_sound(path: String, sound_name: String, num_sounds: int):
 	var sound = path + sound_name + str(randi_range(1, num_sounds)) + ".wav"
 	AudioManager.play_sfx(sound)
-	if sound_name == "Shoot":
-		print(sound)
 
 
 func _slash_sound_play():
